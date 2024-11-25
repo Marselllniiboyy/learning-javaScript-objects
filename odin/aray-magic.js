af7e9401804057a6389b5eeda4f8fe875fc1083a
Made <122382388+Marselllniiboyy@users.noreply.github.com>
2024-11-26 01:01:59 +0800

    Method of filter and map has done

--- odin/aray-magic.js
+++ odin/aray-magic.js
@@ -3,13 +3,23 @@
 function sumOfTripledEvens() {
   // sum of everi even number like 2+3 4+3
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let evenNumbersOnly = [];
   let sumEvent = 0;
-  for (num of numbers) {
-    if (num % 2 === 0) {
-      let tripelEvent = num * 3;
-      sumEvent += tripelEvent;
+  // for (num of numbers) {
+  //   if (num % 2 === 0) {
+  //     let tripelEvent = num * 3;
+  //     sumEvent += tripelEvent;
+  //   }
+  // }
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      const tripelNumber = numbers[i] + 3;
+      evenNumbersOnly.push(numbers[i]);
+      sumEvent += tripelNumber;
     }
   }
+  console.log(...evenNumbersOnly);
+  console.log(sumEvent);
   return sumEvent;
 }
 
